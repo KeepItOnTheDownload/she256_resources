a0214a6f8fb61c408573d64a99825f1d3276c5c7
Alexis S <a.e.schotten@gmail.com>
2020-05-22 11:33:38 -0400

    files

---
+++ 15-Algorithmic-Trading/2/Activities/01-Stu_Trading_Functions/Unsolved/jarvis.py
@@ -0,0 +1,51 @@
+5. def evaluate_metrics():
+    """Generates evaluation metrics from backtested signal data."""
+    return
+
+
+3.def generate_signals():
+    """Generates trading signals for a given dataset."""
+    return
+
+
+2. def fetch_data():
+    """Fetches the latest prices."""
+    return
+
+
+6. def build_dashboard():
+    """Build the dashboard."""
+    return
+
+
+7. def update_dashboard():
+    """Updates the dashboard."""
+    return
+
+
+5. def execute_backtest():
+    """Backtests signal data."""
+    return
+
+
+1. def initialize(cash=None):
+    """Initialize the dashboard, data storage, and account balances."""
+    return
+
+
+6.def execute_trade_strategy():
+    """Makes a buy/sell/hold decision."""
+    return
+
+
+4. def main():
+    """Main Event Loop."""
+    # @TODO: Call the functions above in the correct order to execute a trade
+    return
+
+initialize 
+fetch data 
+generate signals
+main 
+execute backtest 
+evaluate the metrics 
